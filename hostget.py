7d5aaee8a01e2c2547c57882c161cbe90748f999
Dead Numbers <dead.nmbr@gmail.com>
2016-03-08 11:21:27 +0200

    hosts range generator

---
+++ hostget.py
@@ -0,0 +1,18 @@
+#/usr/bin/env python
+
+import sys
+
+def ips(start, end):
+    import socket, struct
+    start = struct.unpack(">I", socket.inet_aton(start))[0]
+    end = struct.unpack(">I", socket.inet_aton(end))[0]
+    return [socket.inet_ntoa(struct.pack(">I", i)) for i in range(start, end)]
+
+hosts = ips(sys.argv[1], sys.argv[2])
+
+with open ('hosts', 'a') as f:
+    for line in hosts:
+        f.write("{}\n".format(line))
+
+print("Done.")
+
